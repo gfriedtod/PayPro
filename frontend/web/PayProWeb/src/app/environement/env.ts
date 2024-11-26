cf9f8c304e4c1e89ec1029c83f5ae8d0eca6695e
gfriedtod <wilfried.wayou@2026.ucac-icam.com>
2024-11-26 13:57:15 +0100

    init

--- frontend/web/PayProWeb/src/app/environement/env.ts
+++ frontend/web/PayProWeb/src/app/environement/env.ts
@@ -2,7 +2,7 @@ import {OrganisationDto} from '../model/OrganisationDto';
 
 export const environment = {
   production: false,
-  api: 'http://localhost:8080',
+  api: 'https://paypro-22iw.onrender.com/',
   SUPABASE_URL: 'https://lriiektzcaxczrhzymgg.supabase.co',
   SUPABASE_KEY:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxyaWlla3R6Y2F4Y3pyaHp5bWdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE0NTI4OTksImV4cCI6MjAzNzAyODg5OX0.oJXiQ-XyA6Cr3DTc2sewL0-028-OZ2SYxTIneuqKAB0',
